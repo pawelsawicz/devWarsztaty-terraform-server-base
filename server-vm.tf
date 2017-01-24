c3fbab021c86414a39b50680c14d4e3e5b1d29e8
pawelsawicz <pawel@sawicz.eu>
2017-01-24 23:40:51 +0000

    Add variables to project

--- server-vm.tf
+++ server-vm.tf
@@ -1,11 +1,16 @@
+variable "base_server_ami" {
+    type = "string"
+    default = "ami-48227e2e"
+}
+
 provider "aws" {
-  access_key = ""
-  secret_key = ""
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
   region = "eu-west-1"
 }
 
 resource "aws_instance" "ns-1" {
-  ami = "ami-48227e2e"
+  ami = "${var.base_server_ami}"
   instance_type = "t2.small"
   key_name = "testkeypair1"
 
